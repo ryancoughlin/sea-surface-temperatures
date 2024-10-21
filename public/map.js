0ccbda92bb69f053765e5efe571915408ecaa1d3
Ryan Coughlin <ryan@ryancoughlin.com>
2024-10-21 11:06:36 -0400

    misc commit working sst py and display on map

--- public/map.js
+++ public/map.js
@@ -1,93 +1,180 @@
-// Remove the import statement
-// import * as L from 'leaflet';
-
-console.log('Map script loaded');
+mapboxgl.accessToken = 'pk.eyJ1Ijoic25vd2Nhc3QiLCJhIjoiY2plYXNjdTRoMDhsbDJ4bGFjOWN0YjdzeCJ9.fM2s4NZq_LUiTXJxsl2HbQ';
 
 document.addEventListener('DOMContentLoaded', initMap);
 
 function initMap() {
-    console.log('Initializing map')
-    
-    if (typeof L === 'undefined') {
-        console.error('Leaflet is not loaded. Make sure to include Leaflet library in your HTML.')
-        return
-    }
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        bounds: [
+            [-72.22598321024519, 40.148157450000284],
+            [-68.62114278975481, 43.401843566726924]
+        ],
+        minZoom: 5,
+        maxZoom: 10
+    });
 
-    const bounds = L.latLngBounds(
-        L.latLng(40.15, -72.23),
-        L.latLng(43.40, -68.62)
-    )
-
-    const map = L.map('map', {
-        center: bounds.getCenter(),
-        zoom: 7,
-        minZoom: 7,
-        maxZoom: 10,
-        maxBounds: bounds,
-        maxBoundsViscosity: 1.0
-    })
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
-
-    const images = {
-        5: L.imageOverlay('capecod_sst_5.png', bounds),
-        8: L.imageOverlay('capecod_sst_8.png', bounds),
-        10: L.imageOverlay('capecod_sst_12.png', bounds)
-    }
-
-    const info = createInfoControl()
-    info.addTo(map)
-
-    function updateImage() {
-        const zoom = map.getZoom()
-        const imageToShow = getImageForZoom(zoom)
-
-        map.eachLayer(layer => {
-            if (layer instanceof L.ImageOverlay) map.removeLayer(layer)
-        })
-        if (imageToShow) imageToShow.addTo(map).setOpacity(0.7)
-
-        info.update()
-        updateMapInfo()
-    }
-
-    map.on('zoomend', updateImage)
-    updateImage()
-    map.fitBounds(bounds)
+    map.on('load', () => {
+        addControls(map);
+        setupImageOverlay(map);
+        setupGeoJSONLayer(map);
+        setupTemperatureDisplay(map);
+        addRegionLayers(map);
+    });
 }
 
-function createInfoControl() {
-    const info = L.control()
-    info.onAdd = function () {
-        this._div = L.DomUtil.create('div', 'info')
-        this.update()
-        return this._div
+function addControls(map) {
+    map.addControl(new mapboxgl.ScaleControl({
+        maxWidth: 80,
+        unit: 'imperial'
+    }), 'bottom-right');
+
+    map.addControl(new mapboxgl.NavigationControl({
+        showZoom: true
+    }), 'bottom-right');
+}
+
+function setupImageOverlay(map) {
+    map.on('zoomend', () => updateImageOverlay(map));
+    updateImageOverlay(map);
+}
+
+function updateImageOverlay(map) {
+    const zoom = map.getZoom();
+    const imageUrl = getImageUrlForZoom(zoom);
+
+    if (map.getSource('sst-image')) {
+        map.removeLayer('sst-layer');
+        map.removeSource('sst-image');
     }
-    info.update = function () {
-        const zoom = map.getZoom()
-        const activeTile = getActiveTile(zoom)
-        this._div.innerHTML = `<h4>Map Info</h4><b>Zoom level:</b> ${zoom}<br><b>Active tile:</b> ${activeTile}`
+
+    if (imageUrl) {
+        map.addSource('sst-image', {
+            type: 'image',
+            url: imageUrl,
+            coordinates: [
+                [-72.22598321024519, 43.401843566726924],
+                [-68.62114278975481, 43.401843566726924],
+                [-68.62114278975481, 40.148157450000284],
+                [-72.22598321024519, 40.148157450000284]
+            ]
+        });
+
+        map.addLayer({
+            id: 'sst-layer',
+            type: 'raster',
+            source: 'sst-image',
+            paint: {
+                'raster-opacity': 0.7
+            }
+        });
     }
-    return info
 }
 
-function getImageForZoom(zoom) {
-    if (zoom >= 5 && zoom < 8) return images[5]
-    if (zoom >= 8 && zoom < 10) return images[8]
-    if (zoom >= 10) return images[10]
-    return null
+function getImageUrlForZoom(zoom) {
+    if (zoom >= 5 && zoom < 8) return './capecod_sst_5.png';
+    if (zoom >= 8 && zoom < 10) return './capecod_sst_8.png';
+    if (zoom >= 10) return './capecod_sst_10.png';
+    return null;
 }
 
-function getActiveTile(zoom) {
-    if (zoom >= 5 && zoom < 8) return 5
-    if (zoom >= 8 && zoom < 12) return 8
-    if (zoom >= 12) return 12
-    return 'None'
+function setupGeoJSONLayer(map) {
+    map.addSource('sst-data', {
+        type: 'geojson',
+        data: './sst_data.geojson'
+    });
+
+    map.addLayer({
+        id: 'sst-points',
+        type: 'circle',
+        source: 'sst-data',
+        paint: {
+            'circle-radius': 5,
+            'circle-opacity': 0,
+            'circle-stroke-width': 1,
+            'circle-stroke-opacity': 0
+        }
+    });
 }
 
-function updateMapInfo() {
-    const mapInfo = document.getElementById('mapInfo')
-    const zoom = map.getZoom()
-    const activeTile = getActiveTile(zoom)
-    mapInfo.innerHTML = `<h4>Map Info</h4><b>Zoom level:</b> ${zoom}<br><b>Active tile:</b> ${activeTile}`
+function setupTemperatureDisplay(map) {
+    const tempInfo = document.getElementById('temp-info');
+
+    map.on('mousemove', 'sst-points', (e) => {
+        const features = map.queryRenderedFeatures(e.point, { layers: ['sst-points'] });
+        if (!features.length) {
+            tempInfo.textContent = 'Temperature: --°F';
+            return;
+        }
+        const feature = features[0];
+        tempInfo.textContent = `Temperature: ${feature.properties.sst_F.toFixed(2)}°F`;
+    });
+
+    map.on('mouseleave', 'sst-points', () => {
+        tempInfo.textContent = 'Temperature: --°F';
+    });
+}
+
+function addRegionLayers(map) {
+    Object.values(REGIONS).forEach(region => {
+        const { slug, name, coordinates } = region;
+        const { minLon, minLat, maxLon, maxLat } = coordinates;
+
+        // Add border layer
+        map.addLayer({
+            id: `${slug}-border`,
+            type: 'line',
+            source: {
+                type: 'geojson',
+                data: {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Polygon',
+                        coordinates: [[
+                            [minLon, minLat],
+                            [maxLon, minLat],
+                            [maxLon, maxLat],
+                            [minLon, maxLat],
+                            [minLon, minLat]
+                        ]]
+                    }
+                }
+            },
+            paint: {
+                'line-color': '#888',
+                'line-width': 2
+            }
+        });
+
+        // Add text label
+        map.addLayer({
+            id: `${slug}-label`,
+            type: 'symbol',
+            source: {
+                type: 'geojson',
+                data: {
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [(minLon + maxLon) / 2, (minLat + maxLat) / 2]
+                    },
+                    properties: {
+                        name: name
+                    }
+                }
+            },
+            layout: {
+                'text-field': ['get', 'name'],
+                'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+                'text-size': 12,
+                'text-anchor': 'center',
+                'text-allow-overlap': true
+            },
+            paint: {
+                'text-color': '#333',
+                'text-halo-color': '#fff',
+                'text-halo-width': 2
+            }
+        });
+    });
 }
---
+++ public/regions.js
@@ -0,0 +1,92 @@
+export const REGIONS = {
+  capecod: {
+    id: 6,
+    name: "Cape Cod / Georges Bank",
+    slug: "capecod",
+    category: {
+      slug: "useca",
+      name: "US East Coast / Atlantic"
+    },
+    coordinates: {
+      minLat: 39.5,
+      maxLat: 43.5,
+      minLon: -71.25,
+      maxLon: -65.25
+    }
+  },
+  canyonsoverview: {
+    id: 7,
+    name: "NE Canyons Overview",
+    slug: "canyonsoverview",
+    category: {
+      slug: "useca",
+      name: "US East Coast / Atlantic"
+    },
+    coordinates: {
+      minLat: 36,
+      maxLat: 42,
+      minLon: -77,
+      maxLon: -65
+    }
+  },
+  canyonsnorth: {
+    id: 8,
+    name: "NE Canyons North",
+    slug: "canyonsnorth",
+    category: {
+      slug: "useca",
+      name: "US East Coast / Atlantic"
+    },
+    coordinates: {
+      minLat: 38,
+      maxLat: 42,
+      minLon: -74.25,
+      maxLon: -67
+    }
+  },
+  canyonssouth: {
+    id: 9,
+    name: "NE Canyons South",
+    slug: "canyonssouth",
+    category: {
+      slug: "useca",
+      name: "US East Coast / Atlantic"
+    },
+    coordinates: {
+      minLat: 36,
+      maxLat: 40.5,
+      minLon: -77,
+      maxLon: -71
+    }
+  },
+  ncarolina: {
+    id: 10,
+    name: "North Carolina",
+    slug: "ncarolina",
+    category: {
+      slug: "useca",
+      name: "US East Coast / Atlantic"
+    },
+    coordinates: {
+      minLat: 33,
+      maxLat: 37,
+      minLon: -79,
+      maxLon: -72
+    }
+  },
+  gasc: {
+    id: 11,
+    name: "South Carolina / Georgia",
+    slug: "gasc",
+    category: {
+      slug: "useca",
+      name: "US East Coast / Atlantic"
+    },
+    coordinates: {
+      minLat: 30.5,
+      maxLat: 34.25,
+      minLon: -81.75,
+      maxLon: -75
+    }
+  }
+};
