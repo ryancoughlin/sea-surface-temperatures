d041d6b48e4177c92798327fea5f83aa9855b246
Ryan Coughlin <ryan@ryancoughlin.com>
2024-11-19 23:17:52 -0500

    perf things

--- config/regions.py
+++ config/regions.py
@@ -6,13 +6,6 @@ class Region(TypedDict):
     bounds: list
 
 REGIONS: Dict[str, Region] = {
-    "gulf_of_mexico": {
-        "name": "Gulf of Mexico",
-        "bounds": [
-            [-98.0, 18.0],
-            [-80.0, 31.0]
-        ]
-    },
     "gulf_of_maine": {
         "name": "Gulf of Maine",
         "bounds": [
@@ -27,39 +20,46 @@ REGIONS: Dict[str, Region] = {
             [-65.25, 43.5]
         ]
     },
-    # "ne_canyons": {
-    #     "name": "NE Canyons Overview",
-    #     "bounds": [
-    #         [-77.0, 36.0],
-    #         [-65.0, 42.0]
-    #     ]
-    # },
-    # "carolinas": {
-    #     "name": "Carolinas",
-    #     "bounds": [
-    #         [-79.0, 33.0],
-    #         [-72.0, 37.0]
-    #     ]
-    # },
-    # "sc_ga": {
-    #     "name": "South Carolina and Georgia",
-    #     "bounds": [
-    #         [-81.75, 30.5],
-    #         [-75.0, 34.25]
-    #     ]
-    # },
-    # "florida_overview": {
-    #     "name": "Florida Overview",
-    #     "bounds": [
-    #         [-88.0, 23.0],
-    #         [-77.0, 31.0]
-    #     ]
-    # },
-    # "bahamas": {
-    #     "name": "Bahamas",
-    #     "bounds": [
-    #         [-80.0, 21.5],
-    #         [-74.0, 28.0]
-    #     ]
-    # }
+    "ne_canyons": {
+        "name": "NE Canyons Overview",
+        "bounds": [
+            [-77.0, 36.0],
+            [-65.0, 42.0]
+        ]
+    },
+    "carolinas": {
+        "name": "Carolinas",
+        "bounds": [
+            [-79.0, 33.0],
+            [-72.0, 37.0]
+        ]
+    },
+    "sc_ga": {
+        "name": "South Carolina and Georgia",
+        "bounds": [
+            [-81.75, 30.5],
+            [-75.0, 34.25]
+        ]
+    },
+    "florida_overview": {
+        "name": "Florida Overview",
+        "bounds": [
+            [-88.0, 23.0],
+            [-77.0, 31.0]
+        ]
+    },
+    "bahamas": {
+        "name": "Bahamas",
+        "bounds": [
+            [-80.0, 21.5],
+            [-74.0, 28.0]
+        ]
+    },
+    "gulf_of_mexico": {
+        "name": "Gulf of Mexico",
+        "bounds": [
+            [-98.0, 18.0],
+            [-80.0, 31.0]
+        ]
+    },
 }
