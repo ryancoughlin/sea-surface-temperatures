66ee55897b587a500395e1d539cc84fb17b59a65
Ryan Coughlin <ryan@ryancoughlin.com>
2024-11-25 09:55:44 -0500

    decrease dpi

--- config/settings.py
+++ config/settings.py
@@ -12,29 +12,29 @@ with open(ROOT_DIR / 'color_scale.json', 'r') as f:  # Update color scale path
 IMAGE_SETTINGS = {
     'colors': color_scale['colors'],
     "colormap": "RdYlBu_r",
-    "dpi": 300,
+    "dpi": 150,
 }
 
 SOURCES = {
-    "LEOACSPOSSTL3SnrtCDaily": {
-        "source_type": "erddap",
-        "type": "sst",
-        "name": "LEO ACSPO SST L3S NRT C Daily",
-        "base_url": "https://coastwatch.noaa.gov/erddap/griddap",
-        "dataset_id": "noaacwLEOACSPOSSTL3SnrtCDaily",
-        "variables": ["sea_surface_temperature", "sst_gradient_magnitude"],
-        "lag_days": 2,
-        "color_scale": "RdYlBu_r",
-        "stride": None,
-        "supportedLayers": ["image", "data", "contours"],
-        "metadata": {
-            "cloud-free": "No",
-            "frequency": "Daily",
-            "resolution": "1 mile",
-            "capture": "blends day and night",
-            "description": "Sea surface temperature from NOAA's ACSPO L3S product.",
-        }
-    },
+    # "LEOACSPOSSTL3SnrtCDaily": {
+    #     "source_type": "erddap",
+    #     "type": "sst",
+    #     "name": "LEO ACSPO SST L3S NRT C Daily",
+    #     "base_url": "https://coastwatch.noaa.gov/erddap/griddap",
+    #     "dataset_id": "noaacwLEOACSPOSSTL3SnrtCDaily",
+    #     "variables": ["sea_surface_temperature", "sst_gradient_magnitude"],
+    #     "lag_days": 2,
+    #     "color_scale": "RdYlBu_r",
+    #     "stride": None,
+    #     "supportedLayers": ["image", "data", "contours"],
+    #     "metadata": {
+    #         "cloud-free": "No",
+    #         "frequency": "Daily",
+    #         "resolution": "1 mile",
+    #         "capture": "blends day and night",
+    #         "description": "Sea surface temperature from NOAA's ACSPO L3S product.",
+    #     }
+    # },
     "CMEMS_Global_Currents_Daily": {
         "source_type": "cmems",
         "name": "CMEMS Global Daily Mean Ocean Currents",
@@ -52,7 +52,7 @@ SOURCES = {
             "description": "Ocean surface currents calculated from model outputs.",
         }
     },
-        "BLENDEDsstDNDaily": {
+    "BLENDEDsstDNDaily": {
         "source_type": "erddap",
         "type": "sst",
         "name": "NOAA Geo-polar Blended SST Analysis Day+Night",
@@ -68,7 +68,7 @@ SOURCES = {
             "frequency": "Daily",
             "resolution": "~3.1 miles (0.05°)",
             "description": "Blended sea surface temperature analysis from multiple satellites.",
-            "capture": "Blended day and night"
+            "capture": "Blends day and night"
         }
     },
     # "CMEMS_Global_Temperature_Daily": {
@@ -88,24 +88,24 @@ SOURCES = {
     #         "description": "Sea surface temperature from global model data.",
     #     }
     # },
-    "chlorophyll_oci": {
-        "source_type": "erddap",
-        "name": "Chlorophyll OCI VIIRS Daily (Gap-filled)",
-        "base_url": "https://coastwatch.noaa.gov/erddap/griddap",
-        "dataset_id": "noaacwNPPN20VIIRSDINEOFDaily",
-        "variables": ["chlor_a"],
-        "lag_days": 2,
-        "color_scale": "YlGnBu",
-        "altitude": "[0:1:0]",
-        "type": "chlorophyll",
-        "supportedLayers": ["image", "data", "contours"],
-        "metadata": {
-            "cloud-free": "No",
-            "frequency": "Daily",
-            "resolution": "2.5 miles",
-            "description": "Chlorophyll-a concentration derived from VIIRS.",
-        }
-    },
+    # "chlorophyll_oci": {
+    #     "source_type": "erddap",
+    #     "name": "Chlorophyll OCI VIIRS Daily (Gap-filled)",
+    #     "base_url": "https://coastwatch.noaa.gov/erddap/griddap",
+    #     "dataset_id": "noaacwNPPN20VIIRSDINEOFDaily",
+    #     "variables": ["chlor_a"],
+    #     "lag_days": 2,
+    #     "color_scale": "YlGnBu",
+    #     "altitude": "[0:1:0]",
+    #     "type": "chlorophyll",
+    #     "supportedLayers": ["image", "data", "contours"],
+    #     "metadata": {
+    #         "cloud-free": "No",
+    #         "frequency": "Daily",
+    #         "resolution": "2.5 miles",
+    #         "description": "Chlorophyll-a concentration derived from VIIRS.",
+    #     }
+    # },
     "CMEMS_Global_Waves_Daily": {
         "source_type": "cmems",
         "name": "CMEMS Global Wave Analysis and Forecast",
